256848468ea256d00e8c435a2023b8490ca73c72
Nikko Sanchez <n.sanchez7580@gmail.com>
2024-02-26 12:26:02 -0800

    esm updates

--- esmfold-tests/tests/2024_26_02/kakawaESM_include_ptm.py
+++ esmfold-tests/tests/2024_26_02/kakawaESM_include_ptm.py
@@ -1,4 +1,5 @@
-import sys
+from argparse import ArgumentParser
+from time import sleep
 import os
 import torch
 import esm
@@ -19,19 +20,23 @@ def set_chunk_size_based_on_length(length):
         return 64
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <fasta_file_path> <output_directory>")
-        sys.exit(1)
-
-    fasta_file_path = sys.argv[1]
-    output_directory = sys.argv[2]
+    parser = ArgumentParser(prog="Python script that runs ESM on kakawa")
+    parser.add_argument("-i", "--input_fasta", type=str, required=True, help="Input FASTA file")
+    parser.add_argument("-o", "--output_directory", type=str, default="ESM_kakawa_results", help="Output directory")
+    args = parser.parse_args()
+    INFILE:str = args.input_fasta
+    if not os.path.exists(INFILE):
+        print(f"Error: Input file {INFILE} does not exist.")
+        exit(1)
+    OUTDIR:str = args.output_directory
+    if os.path.exists(OUTDIR):
+        print(f"Warning: Output directory {OUTDIR} detected. Replacing its contents in 3 seconds...")
+        sleep(3)
+    os.mkdir(OUTDIR)
     
     model = esm.pretrained.esmfold_v1()
     model = model.eval().cuda()
-    proteins = parse_fasta(fasta_file_path)
-
-    # Create output directory if it does not exist
-    os.makedirs(output_directory, exist_ok=True)
+    proteins = parse_fasta(INFILE)
 
     with torch.no_grad():
         for name, sequence in proteins:
@@ -43,5 +48,5 @@ if __name__ == "__main__":
                                  chain_linker="X"*25, # hard-coded
                                  residue_index_offset=512)
             ptm = output["ptm"][0]
-            with open(os.path.join(output_directory, f"{name}_{ptm}.pdb"), "w") as f:
+            with open(os.path.join(OUTDIR, f"{name}_{ptm}.pdb"), "w") as f:
                 f.write(model.output_to_pdb(output)[0])
