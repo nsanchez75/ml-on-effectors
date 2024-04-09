469f0a2397c5209d754a569cce10a3902875b251
Nikko Sanchez <n.sanchez7580@gmail.com>
2024-04-08 22:22:11 -0700

    personal computer updates

---
+++ effectoro-tests/tests/2024_04_08/test_biopython_blastp.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from subprocess import run
+
+from Bio.Blast.Applications import NcbiblastpCommandline
+
+def main():
+  parser = ArgumentParser(prog="test")
+  parser.add_argument("-d", "--ncbi_database", type=str, help="Input NCBI database", required=True)
+  parser.add_argument("-i", "--input_fasta", type=str, help="Input FASTA file for queried genome", required=True)
+  parser.add_argument("-n", "--genome_name", type=str, help="Genome name used for identification", required=True)
+  args = parser.parse_args()
+
+  database:str    = args.ncbi_database
+  fasta:str       = args.input_fasta
+  genome_name:str = args.genome_name
+
+  print(NcbiblastpCommandline(cmd="blastp", database=database, query=fasta, outfmt="6 std qcovs", num_threads=4, out=f"sp_{genome_name}_vs_all.tab"))
+
+
+if __name__ == "__main__":
+  main()
---
+++ effectoro-tests/tests/used_scripts.md
@@ -0,0 +1,7 @@
+# EffectorO Scripts
+
+## 4/8/2024
+
+```bash
+python3 effectorO_lsp.py -i input.fasta -d test.db -n test_name
+```
